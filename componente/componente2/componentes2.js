044a0837c5c0a253cc7e1066635d66d5b2a17fa8
A$AP_Pretty <hdvillatoro@scl.edu.gt>
2024-10-10 13:36:27 -0600

    rm-componentes2

--- componente/componente2/componentes2.js
+++ componente/componente2/componentes2.js
@@ -2,13 +2,13 @@ function componente2() {
     let div = document.createElement('div');
     div.className = "componente2";
 
-    // Estructura del componente
     div.innerHTML = `
-        <h2>Componente 2</h2>
-        <p>Este es el contenido del componente 2.</p>
+        <input type="text" placeholder="Buscar..." class="input-busqueda">
+        <button class="btn-enviar">Enviar</button>
     `;
 
     return div;
 }
 
 export { componente2 };
+
--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import { componente1 } from './componente/componentes1/componentes1.js';
+import { componente2 } from './componente/componente2/componentes2.js';
 
 let DOM = document.querySelector("#root");
 
 // Agregar Componente 1 al DOM
 let comp1 = componente1();
 DOM.appendChild(comp1);
+
+let comp2 = componente2();
+DOM.appendChild(comp2);
