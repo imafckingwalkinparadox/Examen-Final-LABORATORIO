de9bbd8279bd1eb8320e891f4b15d325c3333a0f
A$AP_Pretty <hdvillatoro@scl.edu.gt>
2024-10-10 11:55:04 -0600

    componente1

---
+++ componente/componentes1/componentes.js
@@ -0,0 +1,10 @@
+export function Componente1() {
+    return `
+      <div class="componente1">
+        <h2>Componente 1</h2>
+        <img src="https://www.google.com/imgres?q=brent%20faiyaz&imgurl=https%3A%2F%2Fi1.sndcdn.com%2Fartworks-D5izwUSrPwMYEZ9f-YKDzyw-t500x500.jpg&imgrefurl=https%3A%2F%2Fsoundcloud.com%2Feltrainmusic%2Fbrent-faiyaz-dead-man-walking-el-train-edit&docid=UeHejsyOX3sdmM&tbnid=d0MUHBc5utrPvM&vet=12ahUKEwiYn5elsISJAxU0VzABHU5-EUYQM3oECCEQAA..i&w=500&h=500&hcb=2&ved=2ahUKEwiYn5elsISJAxU0VzABHU5-EUYQM3oECCEQAA" alt="Placeholder Image">
+      </div>
+    `;
+  }
+  
+  
---
+++ main.js
@@ -0,0 +1,3 @@
+import { Componente1 } from './componente/componente_1/componente_1.js';
+
+document.getElementById('componente1').innerHTML = Componente1();
